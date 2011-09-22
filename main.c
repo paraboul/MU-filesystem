ba56d69acc2dad93c23aa0882154774b41af1d19
paraboul <a.catel@weelya.com>
2011-09-22 18:29:20 +0200

    Import 3

--- main.c
+++ main.c
@@ -29,23 +29,34 @@ static const char *entry_file = "/home/para/dev/fuse/list.txt";
 
 static int mufs_getattr(const char *path, struct stat *stbuf)
 {
-  int res = 0;
-  memset(stbuf, 0, sizeof(struct stat));
-  if(strcmp(path, "/") == 0) {
-      stbuf->st_mode = S_IFDIR | 0755;
-      stbuf->st_nlink = 2;
-  }
-  else if (strcmp(path, "/Lost.Girl.avi") == 0) {
-      log_msg("Get attr %s\n", path);
-      stbuf->st_mode = S_IFREG | 0444;
-      stbuf->st_nlink = 1;
-      stbuf->st_size = 1386524358;
-  }
-  else {
-    log_msg("Get attr not found %s\n", path);
-      res = -ENOENT;
+    int res = 0;
+    mu_file_t *mu_file;
+    mu_state_t *state = MU_DATA;
+    
+    memset(stbuf, 0, sizeof(struct stat));
+
+    if (strcmp(path, "/") == 0) {
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+        
+    } else if ((mu_file = hashtbl_seek(state->flist, &path[1])) != NULL) {
+        log_msg("Get attr %s\n", path);
+        
+        if (mu_file->url == NULL) {
+            log_msg("Init file info %s\n", path);
+            mu_file->url    = mu_get_file(mu_file->tag, state->session);
+            mu_file->size   = mu_get_file_size(mu_file->url);
+        }
+        
+        stbuf->st_mode = S_IFREG | 0444;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = mu_file->size;
+        
+    } else {
+        log_msg("Get attr not found %s\n", path);
+        res = -ENOENT;
     }
-  return res;
+    return res;
 }
 
 static int mufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
@@ -56,6 +67,8 @@ static int mufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     (void) fi;
     char url[512];
     char title[512];
+    
+    mu_state_t *state = MU_DATA;
     
     if(strcmp(path, "/") != 0)
       return -ENOENT;
@@ -71,6 +84,18 @@ static int mufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     filler(buf, "..", NULL, 0);
 
     while (fscanf(lst, "%s %s\n", title, url) != EOF) {
+        mu_file_t *mu_file;
+        
+        if ((mu_file = hashtbl_seek(state->flist, title)) == NULL) {
+            mu_file = malloc(sizeof(mu_file_t));
+            mu_file->title = strdup(title);
+            mu_file->tag = strdup(strchr(url, '=')+1);
+            mu_file->url = NULL;
+            mu_file->size = 0;
+            
+            hashtbl_append(state->flist, title, mu_file);
+        }
+        
         filler(buf, title, NULL, 0);
     }
 
@@ -79,7 +104,6 @@ static int mufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 static int mufs_open(const char *path, struct fuse_file_info *fi)
 {
-    int fd;
 
     #if 0
     fd = open("/home/para/dev/fuse/futurama.mkv", fi->flags);
@@ -115,12 +139,17 @@ int mufs_release(const char *path, struct fuse_file_info *fi)
 static int mufs_read(const char *path, char *buf, size_t size, off_t offset,
                     struct fuse_file_info *fi)
 {
+    mu_file_t *mu_file;
+    mu_state_t *state = MU_DATA;
     str_buffer_t buffer;
+    
+    mu_file = hashtbl_seek(state->flist, &path[1]);
+    
     log_msg("Read file : %s - size : %d - offset : %d\n", path, size, offset);
     
     buffer.ptr = (unsigned char *)buf;
 
-    mu_get_range("http://www926.megaupload.com/files/e68ab188370102dd619ac6b9b9ad3731/Lost.Girl.S02E02.VOSTFR.720p.WEB-DL.AAC2.0.H.264-GKS.mkv", offset, size, 1000, &buffer);
+    mu_get_range(mu_file->url, offset, size, mu_file->size, &buffer);
 
     return buffer.length;
 }
@@ -140,7 +169,7 @@ static mu_state_t *mu_init_state()
     
     curl_global_init(CURL_GLOBAL_ALL);
     
-    if ((mu_state->session = mu_login("LezardSpock", "***")) == NULL)
+    if ((mu_state->session = mu_login("LezardSpock", "artefis")) == NULL)
         return NULL;
     
     return mu_state;
